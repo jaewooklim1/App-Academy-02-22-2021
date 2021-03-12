3f2d9440c6c911195e633bd1b054b522551a31c2
Syldys <syldysnya@gmail.com>
2021-03-12 16:24:39 -0500

    No ideas

--- PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -12,18 +12,23 @@ class PolyTreeNode
 
     def parent=(node)
         # debugger
-        # if !node.children.include?(self)
-            p node.children 
-            p "-------------"
-            node.children << self
-            p "!!!!!!!!!!!!!"
-            p node.children
-            p "+++++++++++++"
-            
-            # @parent = node
+        if node.parent.include?(self)
+            node.parent = nil
         end
-        # @parent = node
+
+        if !node.children.include?(self)
+            node.children << self
+        end
+
+        # if node.parent == nil
+
+        # end
+        @parent = node
         
+    end
+
+    def add_child(node3)
+        node3.parent = self
     end
 
     # def inspect
