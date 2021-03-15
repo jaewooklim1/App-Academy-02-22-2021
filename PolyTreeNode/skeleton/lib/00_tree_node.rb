37b26d54d5caa262c338bfeb9346891230555599
jaewooklim1 <jaewooklim1@gmail.com>
2021-03-15 13:32:18 -0400

    testing

--- PolyTreeNode/skeleton/lib/00_tree_node.rb
+++ PolyTreeNode/skeleton/lib/00_tree_node.rb
@@ -1,4 +1,5 @@
 require "byebug"
+require "set"
 
 class PolyTreeNode
 
@@ -39,20 +40,45 @@ class PolyTreeNode
     end
 
 
-    def dfs(target_value)
-        # debugger
-        return nil if self.length < 0
+    # def dfs(target_value)
+    #     # debugger
+    #     return nil if self.length < 0
 
-        mid = self.length / 2
-        case self[mid] <=> target_value
-        when 0
-            return mid
-        when 1
-            return self.take(mid).dfs(target_value)
-        else
-            search_res = self.drop(mid + 1).dfs(target_value)
-            search_res.nil? ? nil : search_res + mid + 1
+    #     mid = self.length / 2
+    #     case self[mid] <=> target_value
+    #     when 0
+    #         return mid
+    #     when 1
+    #         return self.take(mid).dfs(target_value)
+    #     else
+    #         search_res = self.drop(mid + 1).dfs(target_value)
+    #         search_res.nil? ? nil : search_res + mid + 1
+    #     end
+    # end
+
+    def bfs(target_value)
+        debugger
+        queue = [self]
+        visited = Set.new
+        return nil if queue.length == 0
+
+
+        
+        while !queue.empty?
+
+            debugger
+            node = queue.first.value
+            if !visited.include?(node)
+                return node if node == target_value
+                    shifted = queue.shift
+                    childs = shifted.children
+                    childs.map {|child| child.value}
+                    queue << childs
+                    visited << node
+            end       
+
         end
+
     end
 
     def inspect
@@ -65,6 +91,6 @@ class PolyTreeNode
 
 end
 
-need_to_be_checked = [self.value]
-ones_that_has_been_checked = Set.new
+# need_to_be_checked = [self.value]
+# ones_that_has_been_checked = Set.new
 
