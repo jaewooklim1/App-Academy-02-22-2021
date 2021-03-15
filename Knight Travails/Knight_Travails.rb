785717c80019e9d26fa35d2e0dfae3e485124ecb
Syldys <syldysnya@gmail.com>
2021-03-15 17:02:44 -0400

    Add ::valid_moves

--- Knight Travails/Knight_Travails.rb
+++ Knight Travails/Knight_Travails.rb
@@ -12,14 +12,57 @@ class KnightPathFinder
         [pos] << newPos
     end
 
-    def root_node(pos)
+    # def root_node(pos)
         
+    # end
+
+    # def build_move_tree
+    #     self.root_nod = PolyTreeNode.new(pos)
+    # end
+
+    def self.valid_moves(pos)
+        possible_moves = [[-2, -1], [-1, -2], [-2, 1], [-1, 2], [2, 1], [1, 2], [1, -2], [2, -1]]
+        x, y = pos
+        valid_moves = [] 
+
+        possible_moves.each do |subarr| # -2, -1
+            new_x = x + subarr[0] # -2 1
+            new_y = y + subarr[1] # -1 2
+            num_arr = (0..7).to_a
+            if num_arr.include?(new_x) && num_arr.include?(new_y)
+                valid_moves << [new_x, new_y]
+            end
+        end
+        valid_moves
     end
 
-    def build_move_tree
+    # used_pos = []
+    # pos_mov = []
 
-    end
+    # 0 K
+    # 1     K
+    # 2   K   
+    # 3       K
+    # 4
+    # 5     K
+    # 6
+    # 7
+    #   0 1 2 3 4 5 6 7
 
+    #   [0, 0] => [0 + 2, 0 + 1] => [2 + 1, 1 + 2] => [3 - 2, 3 - 1]
+    #   [x +- 2, y +- 1] || [x +- 1, x +- 2] if x.positive? && y.positive?
+
+    #   [1, 2], [2, 1]
+
+    #   [x + 2, y + 1] [x + 2, y - 1] [x - 2, y + 1] [x - 2, y - 1]
+
+    # kpf = KnightPathFinder.new([3, 3])
+    # kpf.find_path([2, 1]) # => [[0, 0], [2, 1]]
+    # p kpf.valid_moves([3, 3]) #[[0, 0], [2, 1], [3, 3]]
+
+
+        
+        
 
 
 
